5a53cd14c9726b46c1744cebaa3eccdc388a7ccd
Tomi Valkeinen <tomi.valkeinen@ti.com>
2015-01-28 15:20:33 +0200

    add sock_fd_write/read

--- common.c
+++ common.c
@@ -1,3 +1,7 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+
 #include "common.h"
 
 static struct timespec timespec_diff(const struct timespec *start,
@@ -28,4 +32,97 @@ uint64_t get_time_elapsed_us(const struct timespec *ts_start, const struct times
 	usecs = res.tv_nsec / 1000 + ((uint64_t)res.tv_sec) * 1000 * 1000;
 
 	return usecs;
+}
+
+/* http://keithp.com/blogs/fd-passing/ */
+ssize_t sock_fd_write(int sock, void *buf, ssize_t buflen, int fd)
+{
+	struct msghdr   msg;
+	struct iovec	iov;
+	union {
+		struct cmsghdr  cmsghdr;
+		char		control[CMSG_SPACE(sizeof (int))];
+	} cmsgu;
+	struct cmsghdr  *cmsg;
+
+	iov.iov_base = buf;
+	iov.iov_len = buflen;
+
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	if (fd != -1) {
+		msg.msg_control = cmsgu.control;
+		msg.msg_controllen = sizeof(cmsgu.control);
+
+		cmsg = CMSG_FIRSTHDR(&msg);
+		cmsg->cmsg_len = CMSG_LEN(sizeof (int));
+		cmsg->cmsg_level = SOL_SOCKET;
+		cmsg->cmsg_type = SCM_RIGHTS;
+
+		int *pfd = (int *) CMSG_DATA(cmsg);
+		*pfd = fd;
+	} else {
+		msg.msg_control = NULL;
+		msg.msg_controllen = 0;
+	}
+
+	return sendmsg(sock, &msg, 0);
+}
+
+/* http://keithp.com/blogs/fd-passing/ */
+ssize_t sock_fd_read(int sock, void *buf, ssize_t bufsize, int *fd)
+{
+	ssize_t	 size;
+
+	if (fd) {
+		struct msghdr   msg;
+		struct iovec	iov;
+		union {
+			struct cmsghdr  cmsghdr;
+			char		control[CMSG_SPACE(sizeof (int))];
+		} cmsgu;
+		struct cmsghdr  *cmsg;
+
+		iov.iov_base = buf;
+		iov.iov_len = bufsize;
+
+		msg.msg_name = NULL;
+		msg.msg_namelen = 0;
+		msg.msg_iov = &iov;
+		msg.msg_iovlen = 1;
+		msg.msg_control = cmsgu.control;
+		msg.msg_controllen = sizeof(cmsgu.control);
+		size = recvmsg (sock, &msg, 0);
+		if (size < 0) {
+			perror ("recvmsg");
+			exit(1);
+		}
+		cmsg = CMSG_FIRSTHDR(&msg);
+		if (cmsg && cmsg->cmsg_len == CMSG_LEN(sizeof(int))) {
+			if (cmsg->cmsg_level != SOL_SOCKET) {
+				fprintf (stderr, "invalid cmsg_level %d\n",
+					 cmsg->cmsg_level);
+				exit(1);
+			}
+			if (cmsg->cmsg_type != SCM_RIGHTS) {
+				fprintf (stderr, "invalid cmsg_type %d\n",
+					 cmsg->cmsg_type);
+				exit(1);
+			}
+
+			int *pfd = (int *) CMSG_DATA(cmsg);
+			*fd = *pfd;
+		} else
+			*fd = -1;
+	} else {
+		size = read (sock, buf, bufsize);
+		if (size < 0) {
+			perror("read");
+			exit(1);
+		}
+	}
+	return size;
 }
--- common.h
+++ common.h
@@ -2,10 +2,16 @@
 #define _COMMON_H_
 
 #include <stdint.h>
+#include <stdlib.h>
 #include <time.h>
 
 /* common.c */
 void get_time_now(struct timespec *ts);
 uint64_t get_time_elapsed_us(const struct timespec *ts_start, const struct timespec *ts_end);
+
+/* send fd to another process */
+ssize_t sock_fd_write(int sock, void *buf, ssize_t buflen, int fd);
+/* receive fd from another process */
+ssize_t sock_fd_read(int sock, void *buf, ssize_t bufsize, int *fd);
 
 #endif
